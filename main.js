7c1c37d1856022a4f67661f51e6ee583094715b4
Denis Olf <denisadnanolf@gmail.com>
2021-04-20 16:17:41 +0200

    fetch

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-console.log(data);
-const releases = data.results;
-console.log(releases);
+// console.log(data);
+// const releases = data.results;
+// console.log(releases);
 
 
-const tbody = document.getElementById("my-list");
- releases.forEach(function (release) {
-     const tr = document.createElement("tr");
-     const td = document.createElement("td")
-     const td2 = document.createElement("td")
-     td.innerHTML = release.title;
-     tr.appendChild(td);
-     td2.innerHTML = release.genre
-     tr.appendChild(td2)
-     tbody.appendChild(tr);
- });
+// const tbody = document.getElementById("my-list");
+//  releases.forEach(function (release) {
+//      const value = release.title.split(" - ")
+//      const tr = document.createElement("tr");
+//      const td = document.createElement("td")
+//      const td1 = document.createElement("td")
+//      const td2 = document.createElement("td")
+//      td.innerHTML = value[0];
+//      tr.appendChild(td);
+//      td1.innerHTML = value[1];
+//      tr.appendChild(td1);
+//      td2.innerHTML = release.genre
+//      tr.appendChild(td2)
+//      tbody.appendChild(tr);
+//  });
 
- let str = td
- // Splitt Artist and release title 
-//  function myFunction() {
-//   let str = "How are you doing today?";
-//   var res = str.split("o");
-//   document.getElementById("demo").innerHTML = res;
+//  // Splitt Artist and release title 
+// //  function myFunction() {
+// //   let str = "How are you doing today?";
+// //   var res = str.split("o");
+// //   document.getElementById("demo").innerHTML = res;
 
-// const countries = [];
-// releases.forEach(function (release) {
-//     countries.push(release.country);
-// })
-// console.log(countries);
+// // const countries = [];
+// // releases.forEach(function (release) {
+// //     countries.push(release.country);
+// // })
+// // console.log(countries);
 
 function showHide(event) {
 let moreText = document.getElementById("moreText");
@@ -45,4 +48,40 @@ button.addEventListener("click", function (event) {
     showHide(event);
 });
 
-fetch
+
+fetch("https://api.discogs.com/database/search?token=CAZzhmuKmjlimgcwqBbGmFwXWKwXKpUZZdxBaCBD&type=release&page=200&per_page=50")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((releases) => {
+        console.log(releases);
+         let myReleases = releases.results
+        displayData(myReleases);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+// console.log(data);
+
+const showError = (er) => {
+    document.getElementById("error").innerHTML = err;
+};
+
+const displayData = (releases) => {
+    const tbody = document.getElementById("my-list");
+ releases.forEach(function (release) {
+     const value = release.title.split(" - ")
+     const tr = document.createElement("tr");
+     const td = document.createElement("td")
+     const td1 = document.createElement("td")
+     const td2 = document.createElement("td")
+     td.innerHTML = value[0];
+     tr.appendChild(td);
+     td1.innerHTML = value[1];
+     tr.appendChild(td1);
+     td2.innerHTML = release.genre
+     tr.appendChild(td2)
+     tbody.appendChild(tr);
+ });
+}
