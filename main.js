53f92cf85b24cac984322d9362c87227cbb97c4d
Denis Olf <denisadnanolf@gmail.com>
2021-04-21 11:58:30 +0200

    content page including results of the api

--- main.js
+++ main.js
@@ -49,43 +49,43 @@ button.addEventListener("click", function (event) {
 });
 
 
-fetch("https://api.discogs.com/database/search?token=CAZzhmuKmjlimgcwqBbGmFwXWKwXKpUZZdxBaCBD&type=release&page=200&per_page=50")
-    .then((response) => {
-        console.log(response);
-        return response.json();
-    })
-    .then((releases) => {
-        console.log(releases);
-         let myReleases = releases.results
-        displayData(myReleases);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-// console.log(data);
+// fetch("https://api.discogs.com/database/search?token=CAZzhmuKmjlimgcwqBbGmFwXWKwXKpUZZdxBaCBD&type=release&page=200&per_page=50")
+//     .then((response) => {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then((releases) => {
+//         console.log(releases);
+//          let myReleases = releases.results
+//         displayData(myReleases);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+// // console.log(data);
 
-const showError = (er) => {
-    document.getElementById("error").innerHTML = err;
-};
+// const showError = (err) => {
+//     document.getElementById("error").innerHTML = err;
+// };
 
-const displayData = (releases) => {
-    const tbody = document.getElementById("my-list");
- releases.forEach(function (release) {
-     const value = release.title.split(" - ")
-     const tr = document.createElement("tr");
-     const td = document.createElement("td")
-     const td1 = document.createElement("td")
-     const td2 = document.createElement("td")
-     const td3 = document.createElement("td")
-     td.innerHTML = value[0];
-     tr.appendChild(td);
-     td1.innerHTML = value[1];
-     tr.appendChild(td1);
-     td2.innerHTML = release.genre
-     tr.appendChild(td2)
-     td3.innerHTML = release.style
-     tr.appendChild(td3)
-     tbody.appendChild(tr);
- });
-}
+// const displayData = (releases) => {
+//     const tbody = document.getElementById("my-list");
+//  releases.forEach(function (release) {
+//      const value = release.title.split(" - ")
+//      const tr = document.createElement("tr");
+//      const td = document.createElement("td")
+//      const td1 = document.createElement("td")
+//      const td2 = document.createElement("td")
+//      const td3 = document.createElement("td")
+//      td.innerHTML = value[0];
+//      tr.appendChild(td);
+//      td1.innerHTML = value[1];
+//      tr.appendChild(td1);
+//      td2.innerHTML = release.genre
+//      tr.appendChild(td2)
+//      td3.innerHTML = release.style
+//      tr.appendChild(td3)
+//      tbody.appendChild(tr);
+//  });
+// }
 
--- main2.js
+++ main2.js
@@ -1,36 +1,39 @@
-console.log(data);
-const releases = data.results;
-console.log(releases);
+fetch("https://api.discogs.com/database/search?token=CAZzhmuKmjlimgcwqBbGmFwXWKwXKpUZZdxBaCBD&type=release&page=200&per_page=50")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((releases) => {
+        console.log(releases);
+         let myReleases = releases.results
+        displayData(myReleases);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+// console.log(data);
 
+const showError = (err) => {
+    document.getElementById("error").innerHTML = err;
+};
 
-const ul = document.getElementById("my-list");
+const displayData = (releases) => {
+    const tbody = document.getElementById("my-list");
  releases.forEach(function (release) {
-     const li = document.createElement("li");
-     li.innerHTML = release.title;
-     ul.appendChild(li);
+     const value = release.title.split(" - ")
+     const tr = document.createElement("tr");
+     const td = document.createElement("td")
+     const td1 = document.createElement("td")
+     const td2 = document.createElement("td")
+     const td3 = document.createElement("td")
+     td.innerHTML = value[0];
+     tr.appendChild(td);
+     td1.innerHTML = value[1];
+     tr.appendChild(td1);
+     td2.innerHTML = release.genre
+     tr.appendChild(td2)
+     td3.innerHTML = release.style
+     tr.appendChild(td3)
+     tbody.appendChild(tr);
  });
-
- const ul2 = document.getElementById("my-list");
- releases.forEach(function (release) {
-     const li = document.createElement("li");
-     li.innerHTML = release.style;
-     ul2.appendChild(li);
-});
-
-
-function showHide(event) {
-let moreText = document.getElementById("moreText");
-let buttonID = event.target.id;
-
-    if (!moreText.classList.contains("show")) {
-        moreText.classList.add("show");
-            document.getElementById(buttonID).innerHTML = "show less...";
-    } else {
-        moreText.classList.remove("show");
-            document.getElementById(buttonID).innerHTML = "show more...";
-    }
 }
-let button = document.getElementById("showMoreButton");
-button.addEventListener("click", function (event) {
-    showHide(event);
-});
